5cebba2c6b3d39dfe0b20aca020f767209ed7e77
tolgasumer <tolga.sumer@tedu.edu.tr>
2019-07-04 09:04:37 +0300

    join channel + sa.mp3

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async message => {
+    if (message.content.indexOf(config.prefix) !== 0) return; // ignore any message that does not start with our prefix
+
+    // command = say
+    // args = ["Is", "this", "real", "life?"]
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        const m = await message.channel.send("hallediliyor...");
+        m.edit(`komut ayıkma sürem ${m.createdTimestamp - message.createdTimestamp}ms, servera pingim ${Math.round(client.ping)}ms`);
+    }
+
+    if (command === "gel") {
+        message.channel.send('sa');
+        voiceChannel = message.member.voiceChannel;
+        connection = await voiceChannel.join();
+        await connection.playFile('./audio/sa.mp3');
+    }
+});
+
+client.login(config.token);
