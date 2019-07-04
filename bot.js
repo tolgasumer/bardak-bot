35fae65bd741e41bb848bb0ab50f679641f7794d
tolgasumer <tolga.sumer@tedu.edu.tr>
2019-07-04 20:26:20 +0300

    ihsan gulme

--- bot.js
+++ bot.js
@@ -25,6 +25,7 @@ client.on('message', async message => {
         voiceChannel = message.member.voiceChannel;
         connection = await voiceChannel.join();
         await connection.playFile('./audio/sa.mp3');
+        await connection.playFile('./audio/gul.ogg');
     }
 });
 
