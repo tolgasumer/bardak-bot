afaea2eb6eefce830daafdc817207544fa46791c
tolgasumer <tolga.sumer@tedu.edu.tr>
2020-03-08 19:38:17 +0300

    +hava.mp3

--- bot.js
+++ bot.js
@@ -85,6 +85,12 @@ client.on('message', async message => {
             await connection.play('./audio/ol.mp3');
         }
     }
+    if (command === "hava") {
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            await connection.play('./audio/hava.mp3');
+        }
+    }
     if (command === "uza") {
         //voiceChannel = await message.member.voice.channel;
         if (message.guild.me.voice.channel.id === message.member.voice.channel.id) {
