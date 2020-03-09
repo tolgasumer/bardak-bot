be7f2337da83d673e5a2c93fe1b137570d03bed8
tolgasumer <tolga.sumer@tedu.edu.tr>
2020-03-09 04:11:36 +0300

    var Long = require("long");

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require("./config.json");
+var Long = require("long");
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
