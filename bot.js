47dc1c66a4a6cf7b6c642d9367c6880f641b1aca
Tolga Sümer <tolga.sumer@tedu.edu.tr>
2020-04-06 19:28:25 +0300

    client.user.setPresence({

--- bot.js
+++ bot.js
@@ -8,7 +8,13 @@ const ytdl = require('ytdl-core');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setStatus("invisible");
+    client.user.setPresence({
+        game: { 
+            name: 'oYunYöNetıCısı - conter strıek 1.5',
+            type: 'PLAYING'
+        },
+        status: 'idle'
+    })
 
     setInterval(sendSoz, 55 * 60000); // sendSoz every 55mins
     setInterval(covidAnons, 60 * 60000); // covidAnons every 60mins
